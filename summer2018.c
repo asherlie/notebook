0f194de708f827d2955bfb965da86d1262685c8f
Asher Lieber <asherlie@buffalo.edu>
2018-06-11 13:15:47 -0400

    initial commit

---
+++ summer2018.c
@@ -0,0 +1,60 @@
+#if 0
+/*
+notes can be anywhere within this comment
+and will be pretty printed
+*/
+#endif
+int nl = __LINE__-4;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int mtime(char* path){
+      struct stat att;
+      stat(path, &att);
+      return att.st_mtime;
+}
+
+int main(int argc, char* argv[]){
+      if(*argv[0] == '.')argv[0] += 2;
+      if(mtime(argv[0]) < mtime(__FILE__)){
+            printf("recompile \"%s\" before running\n", __FILE__);
+            return -1;
+      }
+      char fill = ' ';
+      if(argc > 1)fill = *argv[1];
+      FILE* fp = fopen(__FILE__, "r");
+      fseek(fp, 9, SEEK_SET);
+      size_t sz = 0;
+      char* txt[nl]; unsigned char n=0, ml=0;
+      int len;
+      while(1){
+            char* ln = NULL;
+            sz = 0;
+            if((len = getline(&ln, &sz, fp)) == EOF || strstr(ln, "*/")){
+                  free(ln);
+                  break;
+            }
+            ln[len-1] = '\0';
+            txt[n++] = ln;
+            if(len > ml)ml = len;
+      }
+      putchar(' ');
+      for(int i = 0; i < ml; ++i){
+            putchar('-');
+      }
+      putchar('\n');
+      for(int i = 0; i < n; ++i){
+            printf("|%s", txt[i]);
+            for(int j = strlen(txt[i])+1; j < ml; ++j)putchar(fill);
+            puts("|");
+            free(txt[i]);
+      }
+      putchar(' ');
+      for(int i = 0; i < ml; ++i){
+            putchar('-');
+      }
+      putchar('\n');
+      fclose(fp);
+}
